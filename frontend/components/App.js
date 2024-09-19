a8f0f65b604500696948bae00199ea5b4e27daaf
Conner Krenzer <krenzer84@gmail.com>
2024-09-19 11:01:30 -0400

    cleanup

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,72 +1,80 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import * as yup from 'yup'
-import schemas from '../../shared/schemas'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import * as yup from "yup";
+import schemas from "../../shared/schemas";
+// 👉 TASK 2 
+import text from "../i18n/index.json";
 
-/*
-  👉 TASK 2
+const getInitialValues = () => ({
+  username: "",
+  favLanguage: "",
+  favFood: "",
+  agreement: false,
+});
+const getInitialValidation = () => ({
+  username: "",
+  favLanguage: "",
+  favFood: "",
+  agreement: "",
+});
 
-  Implement internationalization!
-
-  This is commonly done using libraries such as `react-i18next` or `react-intl`
-  But today you will do it "by hand" using the JSON file inside the `i18n` folder
-*/
-
-const getInitialValues = () => ({ username: '', favLanguage: '', favFood: '', agreement: false })
-const getInitialValidation = () => ({ username: '', favLanguage: '', favFood: '', agreement: '' })
-
-export default function App({ lang = 'en' }) {
+export default function App({ lang = "en" }) {
   // ❗ IMPORTANT
   // ✨ The `lang` prop determines which language is used in the UI
   // ✨ If lang is "en" the interface should render in English
   // ✨ If lang is "esp" the interface should render in Spanish
-  const [language, setLanguage] = useState(lang)
-  const [values, setValues] = useState(getInitialValues())
-  const [errors, setErrors] = useState(getInitialValidation())
-  const [success, setSuccess] = useState()
-  const [failure, setFailure] = useState()
-  const [submitAllowed, setSubmitAllowed] = useState(false)
+  const [language, setLanguage] = useState(lang);
+  const [values, setValues] = useState(getInitialValues());
+  const [errors, setErrors] = useState(getInitialValidation());
+  const [success, setSuccess] = useState();
+  const [failure, setFailure] = useState();
+  const [submitAllowed, setSubmitAllowed] = useState(false);
+
+  const texts = text[language];
 
   useEffect(() => {
-    schemas.userSchema.isValid(values).then(setSubmitAllowed)
-  }, [values])
+    schemas.userSchema.isValid(values).then(setSubmitAllowed);
+  }, [values]);
 
-  const onChange = evt => {
-    let { type, name, value, checked } = evt.target
-    value = (type == 'checkbox' ? checked : value)
-    setValues({ ...values, [name]: value })
-    yup.reach(schemas.userSchema, name).validate(value)
-      .then(() => setErrors(e => ({ ...e, [name]: '' })))
-      .catch(err => setErrors(e => ({ ...e, [name]: err.errors[0] })))
-  }
+  const onChange = (evt) => {
+    let { type, name, value, checked } = evt.target;
+    value = type == "checkbox" ? checked : value;
+    setValues({ ...values, [name]: value });
+    yup
+      .reach(schemas.userSchema, name)
+      .validate(value)
+      .then(() => setErrors((e) => ({ ...e, [name]: "" })))
+      .catch((err) => setErrors((e) => ({ ...e, [name]: err.errors[0] })));
+  };
 
-  const onSubmit = evt => {
-    evt.preventDefault()
-    setSubmitAllowed(false)
-    axios.post('http://localhost:9009/api/register', values)
-      .then(res => {
-        console.log(res.data)
-        setValues(getInitialValues())
-        setSuccess(res.data.message)
-        setFailure()
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    setSubmitAllowed(false);
+    axios
+      .post("http://localhost:9009/api/register", values)
+      .then((res) => {
+        console.log(res.data);
+        setValues(getInitialValues());
+        setSuccess(res.data.message);
+        setFailure();
       })
-      .catch(err => {
-        console.log(err.message)
-        console.log(err?.response?.data?.message)
-        setFailure(err?.response?.data?.message)
-        setSuccess()
+      .catch((err) => {
+        console.log(err.message);
+        console.log(err?.response?.data?.message);
+        setFailure(err?.response?.data?.message);
+        setSuccess();
       })
       .finally(() => {
-        setSubmitAllowed(true)
-      })
-  }
+        setSubmitAllowed(true);
+      });
+  };
 
   return (
     <div>
       <h2>
-        Create an Account
-        <span onClick={() => setLanguage(language === 'en' ? 'esp' : 'en')}>
-          {language === 'en' ? ' 🇺🇸' : ' 🇪🇸'}
+        {texts.TEXT_HEADING_CREATE_ACCOUNT}
+        <span onClick={() => setLanguage(language === "en" ? "esp" : "en")}>
+          {language === "en" ? " 🇺🇸" : " 🇪🇸"}
         </span>
       </h2>
       <form onSubmit={onSubmit}>
@@ -74,49 +82,85 @@ export default function App({ lang = 'en' }) {
         {failure && <h4 className="error">{failure}</h4>}
 
         <div className="inputGroup">
-          <label htmlFor="username">Username:</label>
-          <input id="username" name="username" onChange={onChange} value={values.username} type="text" placeholder="Type username" />
-          {errors.username && <div className="validation">{errors.username}</div>}
+          <label htmlFor="username">{texts.LABEL_USERNAME}</label>
+          <input
+            id="username"
+            name="username"
+            onChange={onChange}
+            value={values.username}
+            type="text"
+            placeholder={texts.PLACEHOLDER_USERNAME}
+          />
+          {errors.username && (
+            <div className="validation">{errors.username}</div>
+          )}
         </div>
 
         <div className="inputGroup">
           <fieldset>
-            <legend>Favorite language:</legend>
+            <legend>{texts.TEXT_FAV_LANG}</legend>
             <label>
-              <input onChange={onChange} type="radio" name="favLanguage" value="javascript" checked={values.favLanguage == 'javascript'} />
+              <input
+                onChange={onChange}
+                type="radio"
+                name="favLanguage"
+                value="javascript"
+                checked={values.favLanguage == "javascript"}
+              />
               JavaScript
             </label>
             <label>
-              <input onChange={onChange} type="radio" name="favLanguage" value="rust" checked={values.favLanguage == 'rust'} />
+              <input
+                onChange={onChange}
+                type="radio"
+                name="favLanguage"
+                value="rust"
+                checked={values.favLanguage == "rust"}
+              />
               Rust
             </label>
           </fieldset>
-          {errors.favLanguage && <div className="validation">{errors.favLanguage}</div>}
+          {errors.favLanguage && (
+            <div className="validation">{errors.favLanguage}</div>
+          )}
         </div>
 
         <div className="inputGroup">
-          <label htmlFor="favFood">Favorite food:</label>
-          <select id="favFood" name="favFood" value={values.favFood} onChange={onChange}>
-            <option value="">-- Select favorite food --</option>
-            <option value="pizza">Pizza</option>
-            <option value="spaghetti">Spaghetti</option>
-            <option value="broccoli">Broccoli</option>
+          <label htmlFor="favFood">{texts.LABEL_FAV_FOOD}</label>
+          <select
+            id="favFood"
+            name="favFood"
+            value={values.favFood}
+            onChange={onChange}
+          >
+            <option value="">{texts.TEXT_OPT_FAV_FOOD_1}</option>
+            <option value="pizza">{texts.TEXT_OPT_FAV_FOOD_2}</option>
+            <option value="spaghetti">{texts.TEXT_OPT_FAV_FOOD_3}</option>
+            <option value="broccoli">{texts.TEXT_OPT_FAV_FOOD_4}</option>
           </select>
           {errors.favFood && <div className="validation">{errors.favFood}</div>}
         </div>
 
         <div className="inputGroup">
           <label>
-            <input id="agreement" type="checkbox" name="agreement" checked={values.agreement} onChange={onChange} />
-            Agree to our terms
+            <input
+              id="agreement"
+              type="checkbox"
+              name="agreement"
+              checked={values.agreement}
+              onChange={onChange}
+            />
+            {texts.LABEL_ACCEPT_TERMS}
           </label>
-          {errors.agreement && <div className="validation">{errors.agreement}</div>}
+          {errors.agreement && (
+            <div className="validation">{errors.agreement}</div>
+          )}
         </div>
 
         <div>
-          <input type="submit" disabled={!submitAllowed} value="Send" />
+          <input type="submit" disabled={!submitAllowed} value={texts.TEXT_SUBMIT} />
         </div>
       </form>
     </div>
-  )
+  );
 }
--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,77 +1,181 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import App from './App'
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
+import text from "../i18n/index.json";
 
-describe('Module 4 Project Tests', () => {
-  describe('English Language', () => {
-    /*
-      👉 TASK 1
-
-      One test is done for you as an example.
-    */
+describe("Module 4 Project Tests", () => {
+  describe("English Language", () => {
+    // 👉 TASK 1
+    let txt = getEntriesByKeyPrefix(text.en, 'TEXT')
+    for (let [key, val] of txt) {
+      test(`${key} is visible`, () => {
+        render(<App />)
+        expect(screen.getByText(val)).toBeVisible()
+      })
+    }
+    // test(`TEXT_HEADING_CREATE_ACCOUNT is visible`, () => {
+    //   render(<App lang="en" />);
+    //   expect(
+    //     screen.getByText(text.en.TEXT_HEADING_CREATE_ACCOUNT)
+    //   ).toBeVisible();
+    // });
+    // test(`TEXT_FAV_LANG_JS is visible`, () => {
+    //   render(<App lang="en" />);
+    //   expect(screen.getByText(text.en.TEXT_FAV_LANG_JS)).toBeVisible();
+    // });
+    // test(`TEXT_FAV_LANG_RUST is visible`, () => {
+    //   render(<App lang="en" />);
+    //   expect(screen.getByText(text.en.TEXT_FAV_LANG_RUST)).toBeVisible();
+    // });
+    // test(`TEXT_OPT_FAV_FOOD_1 is visible`, () => {
+    //   render(<App lang="en" />);
+    //   expect(screen.getByText(text.en.TEXT_OPT_FAV_FOOD_1)).toBeVisible();
+    // });
+    // test(`TEXT_OPT_FAV_FOOD_2 is visible`, () => {
+    //   render(<App lang="en" />);
+    //   expect(screen.getByText(text.en.TEXT_OPT_FAV_FOOD_2)).toBeVisible();
+    // });
+    // test(`TEXT_OPT_FAV_FOOD_3 is visible`, () => {
+    //   render(<App lang="en" />);
+    //   expect(screen.getByText(text.en.TEXT_OPT_FAV_FOOD_3)).toBeVisible();
+    // });
+    // test(`TEXT_OPT_FAV_FOOD_4 is visible`, () => {
+    //   render(<App lang="en" />);
+    //   expect(screen.getByText(text.en.TEXT_OPT_FAV_FOOD_4)).toBeVisible();
+    // });
+    // test(`TEXT_SUBMIT is visible`, () => {
+    //   render(<App lang="en" />);
+    //   expect(screen.getByText(text.en.TEXT_SUBMIT)).toBeVisible();
+    // });
+    // test(`TEXT_FAV_LANG is visible`, () => {
+    //   render(<App lang="en" />);
+    //   expect(screen.getByText(text.en.TEXT_FAV_LANG)).toBeVisible();
+    // });
+    test(`PLACEHOLDER_USERNAME is visible`, () => {
+      render(<App lang="en" />);
+      expect(
+        screen.getByPlaceholderText(text.en.PLACEHOLDER_USERNAME)
+      ).toBeVisible();
+    });
+    test(`LABEL_USERNAME is visible`, () => {
+      render(<App lang="en" />);
+      expect(screen.getByLabelText(text.en.LABEL_USERNAME)).toBeVisible();
+    });
+    test(`LABEL_FAV_FOOD is visible`, () => {
+      render(<App lang="en" />);
+      expect(screen.getByLabelText(text.en.LABEL_FAV_FOOD)).toBeVisible();
+    });
+    test(`LABEL_ACCEPT_TERMS is visible`, () => {
+      render(<App lang="en" />);
+      expect(screen.getByLabelText(text.en.LABEL_ACCEPT_TERMS)).toBeVisible();
+    });
+  });
+  describe("Spanish Language", () => {
+    // 👉 TASK 3
     test(`TEXT_HEADING_CREATE_ACCOUNT is visible`, () => {
-      render(<App lang="en" />)
-      expect(screen.getByText("Create an Account")).toBeVisible()
-    })
-  })
-  describe('Spanish Language', () => {
-    /*
-      👉 TASK 3
-
-      This is done after making the UI multilingual.
-    */
-  })
-  describe('getEntriesByKeyPrefix', () => {
-    test('can extract the correct data', () => {
-    /*
-      👉 TASK 4 part 2
-
-      Implement the function `getEntriesByKeyPrefix` below
-      and then come back here and write a few tests
-      to ensure it works as expected.
-
-      Although it should be noted that commonly,
-      the tests are written _before_ implementing
-      the function being tested.
-    */
-    })
-  })
-})
+      render(<App lang="esp" />);
+      expect(
+        screen.getByText(text.esp.TEXT_HEADING_CREATE_ACCOUNT)
+      ).toBeVisible();
+    });
+    test(`TEXT_FAV_LANG_JS is visible`, () => {
+      render(<App lang="esp" />);
+      expect(screen.getByText(text.esp.TEXT_FAV_LANG_JS)).toBeVisible();
+    });
+    test(`TEXT_FAV_LANG_RUST is visible`, () => {
+      render(<App lang="esp" />);
+      expect(screen.getByText(text.esp.TEXT_FAV_LANG_RUST)).toBeVisible();
+    });
+    test(`TEXT_OPT_FAV_FOOD_1 is visible`, () => {
+      render(<App lang="esp" />);
+      expect(screen.getByText(text.esp.TEXT_OPT_FAV_FOOD_1)).toBeVisible();
+    });
+    test(`TEXT_OPT_FAV_FOOD_2 is visible`, () => {
+      render(<App lang="esp" />);
+      expect(screen.getByText(text.esp.TEXT_OPT_FAV_FOOD_2)).toBeVisible();
+    });
+    test(`TEXT_OPT_FAV_FOOD_3 is visible`, () => {
+      render(<App lang="esp" />);
+      expect(screen.getByText(text.esp.TEXT_OPT_FAV_FOOD_3)).toBeVisible();
+    });
+    test(`TEXT_OPT_FAV_FOOD_4 is visible`, () => {
+      render(<App lang="esp" />);
+      expect(screen.getByText(text.esp.TEXT_OPT_FAV_FOOD_4)).toBeVisible();
+    });
+    test(`TEXT_SUBMIT is visible`, () => {
+      render(<App lang="esp" />);
+      expect(screen.getByText(text.esp.TEXT_SUBMIT)).toBeVisible();
+    });
+    test(`TEXT_FAV_LANG is visible`, () => {
+      render(<App lang="esp" />);
+      expect(screen.getByText(text.esp.TEXT_FAV_LANG)).toBeVisible();
+    });
+    test(`PLACEHOLDER_USERNAME is visible`, () => {
+      render(<App lang="esp" />);
+      expect(
+        screen.getByPlaceholderText(text.esp.PLACEHOLDER_USERNAME)
+      ).toBeVisible();
+    });
+    test(`LABEL_USERNAME is visible`, () => {
+      render(<App lang="esp" />);
+      expect(screen.getByLabelText(text.esp.LABEL_USERNAME)).toBeVisible();
+    });
+    test(`LABEL_FAV_FOOD is visible`, () => {
+      render(<App lang="esp" />);
+      expect(screen.getByLabelText(text.esp.LABEL_FAV_FOOD)).toBeVisible();
+    });
+    test(`LABEL_ACCEPT_TERMS is visible`, () => {
+      render(<App lang="esp" />);
+      expect(screen.getByLabelText(text.esp.LABEL_ACCEPT_TERMS)).toBeVisible();
+    });
+  });
+  describe("getEntriesByKeyPrefix", () => {
+    test("can extract the correct data", () => {
+      // 👉 TASK 4 part 2
+      const obj = {
+        abc_1: "data_abc_1",
+        abc_2: "data_abc_2",
+        xyz_1: "data_xyz_1",
+        abc_3: "data_abc_3",
+      };
+      const expected = [
+        ["abc_1", "data_abc_1"],
+        ["abc_2", "data_abc_2"],
+        ["abc_3", "data_abc_3"],
+      ];
+      const expected2 = [["xyz_1", "data_xyz_1"]];
+      expect(getEntriesByKeyPrefix(obj, "abc")).toEqual(expected);
+      expect(getEntriesByKeyPrefix(obj, "xyz")).toEqual(expected2);
+      expect(getEntriesByKeyPrefix(obj, "foo")).toEqual([]);
+    });
+  });
+});
 function getEntriesByKeyPrefix(obj, keyPrefix) {
   /*
     👉 TASK 4 part 1
-
     Implement a function that takes as first argument an object `obj` such as this:
-
     {
       abc_1: "data_abc_1",
       abc_2: "data_abc_2",
       xyz_1: "data_xyz_1",
       abc_3: "data_abc_3",
     }
-
     and takes as second argument a string `keyPrefix` such as this: "abc"
-
     and returns an array of arrays such as this (for the arguments given in the examples above):
-
     [
       ["abc_1", "data_abc_1"],
       ["abc_2", "data_abc_2"],
       ["abc_3", "data_abc_3"],
     ]
-
     If the function is passed the same `obj` as above but a `keyPrefix` of "xyz" then it would return:
-
     [
       ["xyz_1", "data_xyz_1"],
     ]
-
     If the function is passed the same `obj` as above but a `keyPrefix` of "foo" then it would return the empty array.
-
     The function looks inside the object `obj`, finds all properties whose property names begin
     with the `keyPrefix` given (followed by an underscore), and reorganizes the information before returning it.
     The properties that match the `keyPrefix` are returned inside an array holding key-value-pair sub-arrays.
-
   */
+  return Object.entries(obj).filter(([key]) => key.split("_")[0] === keyPrefix);
 }
